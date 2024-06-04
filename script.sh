78e41bab12d477c1e29629d8bf12992aeb8c0193
Audrius Skema <askema@gmail.com>
2024-06-04 13:31:23 +0100

    Updated script.sh to display job completed message

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 rm fol_1
 rm fol_2
 
@@ -7,3 +8,11 @@ mkdir fol_2
 
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+
+chmod 600 fol_1/*_1.txt fol_1/*_3.txt
+chmod 600 fol_2/*_1.txt fol_2/*_3.txt
+
+chmod 700 fol_1/*_2.txt
+chmod 700 fol_2/*_2.txt
+
+echo "Job completed."
